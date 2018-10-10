b2c47bb749e1212b6fffd929824a13af3ae16861
Adriyadi Savana <adrie4mac@gmail.com>
2018-10-10 07:25:38 +0700

    [UPDATE] Update detail movie

--- Movie/src/main/java/com/adriesavana/movie/model/MovieDetail.kt
+++ Movie/src/main/java/com/adriesavana/movie/model/MovieDetail.kt
@@ -16,4 +16,20 @@ data class MovieDetail(val voteCount: Int,
                        val adult: Boolean,
                        val overview: String,
                        val releaseDate: String)
-    : Serializable
+    : Serializable {
+
+    fun createMovieView() : MovieDetailView {
+        return MovieDetailView(
+                voteCountView = "$voteCount votes",
+                voteAverageView = "$voteAverage/10",
+                titleView = title,
+                posterPathView = posterPath,
+                originalLanguageView = originalLanguage,
+                originalTitleView = originalTitle,
+                backdropPathView = backdropPath,
+                overview = overview,
+                releaseDate = releaseDate
+        )
+    }
+
+}
--- app/src/main/java/com/adriesavana/themoviedb/moviedetail/MovieDetailActivity.kt
+++ app/src/main/java/com/adriesavana/themoviedb/moviedetail/MovieDetailActivity.kt
@@ -1,10 +1,13 @@
 package com.adriesavana.themoviedb.moviedetail
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import com.adriesavana.kit.extension.disposedBy
 import com.adriesavana.movie.model.MovieDetail
 import com.adriesavana.themoviedb.R
 import com.adriesavana.themoviedb.common.base.BaseActivity
+import com.adriesavana.themoviedb.databinding.ActivityMovieDetailBinding
+import com.android.databinding.library.baseAdapters.BR
 
 class MovieDetailActivity : BaseActivity<MovieDetailViewModelType>() {
 
@@ -15,10 +18,15 @@ class MovieDetailActivity : BaseActivity<MovieDetailViewModelType>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_detail)
+        val binding: ActivityMovieDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_movie_detail)
 
         val movieDetail = intent.extras.get(EXTRA_MOVIE) as? MovieDetail
-        viewModel.inputs.onViewLoaded(movieDetail)
+
+        binding.apply {
+            setVariable(BR.movieModel, movieDetail?.createMovieView())
+            executePendingBindings()
+        }
+        //viewModel.inputs.onViewLoaded(movieDetail)
     }
 
     override fun bindViewModel() {
