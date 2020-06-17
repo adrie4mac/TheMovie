70e6e01ff77b8be192552400b491cdb3a02c5dae
Adriyadi Savana <adrie4mac@gmail.com>
2020-06-17 23:18:32 +0700

    Remove movie module from application

--- Movie/src/main/java/com/adriesavana/movie/model/MovieList.kt
+++
@@ -1,6 +0,0 @@
-package com.adriesavana.movie.model
-
-data class MovieList(var isPagination: Boolean = false,
-                     var nextPage: Int = -1,
-                     val totalPagesOfMoview: Int,
-                     val movieList: List<MovieDetail>)
--- Movie/src/main/java/com/adriesavana/movie/repository/MovieRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.adriesavana.movie.repository
-
-import com.adriesavana.movie.model.MovieList
-import io.reactivex.Single
-
-interface MovieRepository {
-
-    fun getMovieList(category: String, page: String): Single<MovieList>
-}
