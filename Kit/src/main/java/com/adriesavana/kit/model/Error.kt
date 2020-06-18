cc92206821b8595096b5828bc260e062057db569
Adriyadi Savana <adrie4mac@gmail.com>
2020-06-18 22:13:42 +0700

    remove kit

--- Kit/src/main/java/com/adriesavana/kit/model/Error.kt
+++
@@ -1,9 +0,0 @@
-package com.adriesavana.kit.model
-
-open class Error(
-        val requestId: String,
-        val status: Int,
-        val data: List<Any>,
-        val errorMessage: String?,
-        val serverProcessTime: Double,
-        val app: String)
