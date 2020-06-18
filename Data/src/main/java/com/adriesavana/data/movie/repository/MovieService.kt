a4f9420f5beb81a0de26e46329d14d8086b02f5c
Adriyadi Savana <adrie4mac@gmail.com>
2020-06-18 21:48:05 +0700

    remove data

--- Data/src/main/java/com/adriesavana/data/movie/repository/MovieService.kt
+++
@@ -1,8 +0,0 @@
-package com.adriesavana.data.movie.repository
-
-import com.adriesavana.data.movie.entity.MovieListEntity
-import io.reactivex.Single
-
-interface MovieService {
-    fun getMovieList(category: String, page: String): Single<MovieListEntity>
-}
